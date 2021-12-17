e30893784ec33ca90e53a30a5bc98df5845004a0
Siamak Mushakhian <siamak.mushakhian@gmail.com>
2021-12-16 21:40:02 -0500

    Text and image sections works very well, but the parameters sections need to be improved

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from wordcloud import STOPWORDS, ImageColorGenerator, WordCloud
 
 st.write("""# What is Word Cloud?
 It is a beautiful visualization format to highlight important textual data points based on the word frequency.
+To generate your word cloud, you need two files, a text file and an image file, and set the parameters.
 """)
 
 def load_image(image_file):
@@ -13,19 +14,16 @@ def load_image(image_file):
 	return img
 
 cols = st.columns(3)
-cols[1].image(load_image('wordcloud.png'), width=250)
+cols[1].image(load_image('wordcloud.png'), width=300)
 
-st.write("""For this type of visualiztion, you need two files:
-## 1) Document
-Which consist the words  """)
-docx_file = st.file_uploader("Upload Document", type=["pdf","docx","txt"])
+st.write("**1) Document**")
+docx_file = st.file_uploader("Upload your text file", type=["docx","txt"])
 if docx_file is not None:
     text = str(docx_file.read(),"utf-8")
 
 
-st.write("""## 2) Image
-It is the desired picture where you want to find your visuzlization on  """)
-image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+st.write("**2) Image**")
+image_file = st.file_uploader("Upload your image file", type=["png","jpg","jpeg"])
 if image_file is not None:
     st.image(load_image(image_file), width=250)
 
@@ -33,10 +31,10 @@ if image_file is not None:
     custom_mask = np.array(load_image(image_file))
 
 
-st.write("""##  Customization
-Choose the below option as like to find your word cloud""")
-
+st.write("**Parameters**")
 cols = st.columns(2)
+wc_width = cols[0].text_input("Width")
+wc_height = cols[1].text_input("Height")
 backgroundcolor_list = ['white', 'black', 'red']
 back_color =cols[0].selectbox("Background color:", backgroundcolor_list)
 
@@ -46,35 +44,37 @@ if stopwords_choice == 'Yes':
 else:
     stopwords = None
 
-if image_file is not None:
-    wc = WordCloud(background_color = back_color, 
+
+st.write("To see your word cloud press visualization button")
+cols = st.columns(3)
+if cols[1].button("Generator"):
+    if image_file is None or docx_file is None:
+        st.error("Please upload your text and image files")
+    else:
+        wc = WordCloud(background_color = back_color, 
                 stopwords = stopwords, 
                 mask = custom_mask, 
                 colormap='gist_rainbow',
-                collocations=False, 
+                collocations=False,
+                width=wc_width,
+                height=wc_height
     #                contour_width = 3, 
     #                contour_color = 'black',
     #                max_font_size = 50, 
-    #                max_words = 5, 
-    #                height = 1000, 
-    #                width = 1000
+    #                max_words = 5
                 )
+        wc.generate(text)
 
-st.write("To see your word cloud press visualization button")
-if st.button("Visualization"):
-    
-    wc.generate(text)
+        image_colors = ImageColorGenerator(custom_mask)
+        wc.recolor(color_func = image_colors)
 
-    image_colors = ImageColorGenerator(custom_mask)
-    wc.recolor(color_func = image_colors)
+        plt.rcParams['figure.figsize'] = [5.0, 5.0]
+        plt.rcParams['figure.dpi'] = 140
+        plt.imshow(wc, interpolation = 'bilinear')
+        plt.axis('off')
+        st.pyplot(plt)
 
-    plt.rcParams['figure.figsize'] = [8.0, 8.0]
-    plt.rcParams['figure.dpi'] = 140
-    plt.imshow(wc, interpolation = 'bilinear')
-    plt.axis('off')
-    plt.show()
-    st.pyplot(plt)
-
-st.write("If you want to save your word cloud, please choose the desired directory:")
-if st.button("save"):
-    wc.to_file('Spruce_cloud.png')
+        # st.write("If you want to download your word cloud, please choose the desired directory:")
+        # cols = st.columns(3)
+        # if cols[1].button("Download"):
+        #     wc.to_file('Spruce_cloud.png')
