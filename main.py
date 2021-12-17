7dad101dbdd742ebd720d6f1c825455675a3eefd
Siamak Mushakhian <siamak.mushakhian@gmail.com>
2021-12-16 19:09:25 -0500

    Define the word cloud and add a picture

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import streamlit as st
+from PIL import Image
+from wordcloud import STOPWORDS, ImageColorGenerator, WordCloud
+
+st.write("""# What is Word Cloud?
+It is a beautiful isualization format to highlight important textual data points.
+""")
+
+cols = st.columns(3)
+cols[1].image(Image.open('wordcloud.png'), width=250)
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+if image_file is not None:
+    # To View Uploaded Image
+    st.image(load_image(image_file), width=250)
